1fc895e16694d8c5929729848d09536fd17ec17d
Lorenzo <lorenzo.f@students.opit.com>
2026-02-21 12:14:07 +0100

    feat: adding base classes to handle Oauth2 login process

---
+++ src/main/java/capstoneProject/Lorenzo/genIz/GenIzApplication.java
@@ -0,0 +1,13 @@
+package capstoneProject.Lorenzo.genIz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenIzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenIzApplication.class, args);
+	}
+
+}
---
+++ src/main/java/capstoneProject/Lorenzo/genIz/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package capstoneProject.Lorenzo.genIz.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(
+            securityConfig -> {
+                securityConfig.requestMatchers("/").permitAll();
+                securityConfig.anyRequest().authenticated();
+            }
+        )
+        .oauth2Login(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults())
+        .build();
+    }
+}
