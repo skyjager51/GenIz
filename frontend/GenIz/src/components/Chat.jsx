bf7cc23b5fe837583b6b249f97e34c1e8167193a
Lorenzo <lorenzo.f@students.opit.com>
2026-03-11 22:45:06 +0100

    style/added chat display and quizzes display

--- frontend/GenIz/src/components/Chat.jsx
+++ frontend/GenIz/src/components/Chat.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { FiFileText } from "react-icons/fi";
 import { IoIosArrowForward } from "react-icons/io";
 
-function Chat({name, isSelected, onSelect, id}){
+function Chat({name, isSelected, onSelect, id, chatSelect}){
 
     const currentClass = isSelected ? 'selected-class' : 'chat-element'
     let chat_id = {id};
-    
+    console.log(chat_id);
+
     return(
-        <div className={currentClass} onClick={onSelect}>
+        <div className={currentClass} onClick={() =>{onSelect(), chatSelect()}}>
             <FiFileText></FiFileText>
             <p> <span>{name}</span> </p>
             <IoIosArrowForward></IoIosArrowForward>
--- frontend/GenIz/src/components/Discussion.jsx
+++ frontend/GenIz/src/components/Discussion.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 
-function Discussion(){
+function Discussion({id, source, quizzes}){
     return(
         <div className="discussion-container">
-            <h1>discussion</h1>
+            <div className="user-pdf-name">
+                <p>{source}</p>
+            </div>
+
+            <div className="quiz-content">
+
+                <div>
+                    <p><span>Id: </span>{id}</p> 
+                    {quizzes.map((quiz, index) => (
+                        <><h3 key={index}>{quiz.question}</h3>
+                        <ul>
+                            {Object.entries(quiz.options).map(([key, value]) => (
+                                <li>
+                                    <p><span>{key}</span> : <span>{value}</span></p>
+                                </li>
+                            ))}
+                        </ul>
+
+                        <p><span>Correct Answer: </span> {quiz.correct_answer}</p>
+                        <p><span>Explanation: </span> {quiz.explanation}</p>
+                        
+                        <br />
+                        </>
+                    ))}
+                </div>
+
+            </div>
+
+            <hr className="end-chat-line"/>
         </div>
     );
 }
