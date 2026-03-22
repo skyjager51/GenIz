9458039715dc7bef118ce29152a7ad24c60f57ab
Lorenzo <lorenzo.f@students.opit.com>
2026-03-22 15:25:12 +0100

    feat/added login mechanism that obligate the user to authenticate to render the page, added call to populate user info section

--- backend/src/main/java/capstoneProject/Lorenzo/genIz/security/SecurityConfig.java
+++ backend/src/main/java/capstoneProject/Lorenzo/genIz/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package capstoneProject.Lorenzo.genIz.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -17,14 +22,33 @@ public class SecurityConfig {
                 securityConfig.anyRequest().authenticated();
             }
         )
-        .oauth2Login(Customizer.withDefaults())
+        //return 401 error code if user is not authenticated
+        .exceptionHandling(exception -> exception
+            .authenticationEntryPoint((request, response, authException) -> {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            }))
+
+        //when user successfully loging, redirect to frontend home
+        .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:5173/", true))
+
         .formLogin(Customizer.withDefaults())
+
         //logout config, by hitting baseUrl/logout the user gets redirected to a default logout page.
         .logout(logout -> {
             logout.logoutSuccessUrl("/")
             .permitAll();
         })
         .csrf(csrf -> csrf.disable())
+
+        .cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(List.of("http://localhost:5173/"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowCredentials(true);
+            return config;
+        }))
+
         .build();
     }
 }
--- frontend/GenIz/src/components/Header.jsx
+++ frontend/GenIz/src/components/Header.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Routes, Route, Link} from "react-router-dom";
 import geniz_logo from "../assets/geniz_logo.png";
+import axios from 'axios';
 
 function Header(){
+
+    //axios request to get user data
+    const api = axios.create({
+        baseURL: 'http://localhost:8080',
+        withCredentials: true
+    });
+
+    //function to make an api call to the backend and retrieve user profile pic and name 
+    const UserDataBlock = () =>{
+        const [data, setData] = useState(null);
+        const [loading, setLoading] = useState(true);
+
+        useEffect(() => {
+
+            const userInfo = async() => {
+                try{
+                    const response = await api.get('/user-info');
+                    setData(response.data);
+
+                } catch (err) {
+                    if (err.response?.status === 401){
+                        window.location.replace('http://localhost:8080/oauth2/authorization/auth0');
+                        return;
+                    }
+                } finally {
+                    setLoading(false);
+                }
+            };
+            userInfo();
+        }, []);
+
+        if(loading) return<div>Loading...</div>;
+        if (!data) return null;
+
+        return(
+            <div className="user-info">
+                <img src={data.userPicture} alt="user image"  className="user-image"/>
+                <p className="username">Hello, <span>{data.userName}</span></p>
+            </div>
+        );
+    }
+    
+
     return(
         <div className="header">
             <div className="header-content">
@@ -11,10 +55,8 @@ function Header(){
                     <p className="app-name">Gen<span id="span">Iz</span></p>
                 </div>
 
-                <div className="user-info">
-                    <img src="" alt="user image"  className="user-image"/>
-                    <p className="username">Hello, username!</p>
-                </div>
+                {/*render current user info*/}
+                <UserDataBlock></UserDataBlock>
 
                 <div className="route-container">
                     {/*top navigation menu, using React Router <Link> components*/}
